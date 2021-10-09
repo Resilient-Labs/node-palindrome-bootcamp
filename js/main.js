66a0e7caa7cec1c6514c69036c114f04d474e74e
Karim Hassan <karimhassan92@gmail.com>
2021-10-08 23:45:42 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,14 @@
-const button = document.querySelector('button')
-const display = document.querySelector('h3')
+const button = document.querySelector("button");
+const display = document.querySelector("h3");
 button.onclick = () => {
-    const result = document.querySelector('input').value
-    fetch(`/api?student=${result}`)
+  const result = document.querySelector("input").value;
+  fetch(`/api?student=${result}`)
     .then((res) => res.json())
     .then((data) => {
-        if(data.palin === 'Yes'){
-            display.innerText = `${result} is indeed a palindrome`
-
-        }
-
-        else{
-            display.innerText = `Unfortunaely, ${result} is not a palindrome`
-        }
-
-
-
-
-
-    })
-
-
-
-}
+      if (data.palin === "Yes") {
+        display.innerText = `${result} is indeed a palindrome`;
+      } else {
+        display.innerText = `Unfortunaely, ${result} is not a palindrome`;
+      }
+    });
+};
