f2851ace59d842b013f2ef916ac60c03c871c783
Alexa Macias <Alexa Macias>
2021-10-11 21:30:15 -0400

    answer

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+
+  fetch(`/api?student=${userName}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("#personName").textContent = data.returnResult
+     // document.querySelector("#personStatus").textContent = data.status
+     // document.querySelector("#personOccupation").textContent = data.currentOccupation
+    });
+
+}
