537fb3d6f643bd3bc7ac836dad4e1b785f2bb083
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-14 02:02:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const pal = document.querySelector('h1')
+
+button.addEventListener('click', checkPal)
+
+// function checkPal() {
+//     let word = input.value
+//     let wordPal = word.split('').reverse().join('')
+//     if( word === wordPal) {
+//         pal.innerText = `${wordPal} is a Palindrome!`
+//     } else {
+//         pal.innerText = `This ${word} is NOT what we want!`
+//     }
+// }
+
+function checkPal() {
+    let submit = input.value
+    fetch(`/api?palindrome=${submit}`)
+      .then(response => response.json())
+      .then((data) => {
+          pal.innerText = `${data.result}`
+      });
+}
