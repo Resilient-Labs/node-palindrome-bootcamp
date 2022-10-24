f329125d968acf87a9f644158839816c57f53945
Hannah Enoy <hannahenoy@gmail.com>
2022-10-24 00:18:35 -0400

    Add files via upload

---
+++ palindrome/js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+
+  fetch(`/api?name=${userName}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    //   document.querySelector("#personName").textContent = data.name
+    //   document.querySelector("#personStatus").textContent = data.status
+    //   document.querySelector("#personOccupation").textContent = data.currentOccupation
+    });
+
+}
