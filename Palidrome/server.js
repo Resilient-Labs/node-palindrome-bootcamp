4844b78dcef156ea8d4d0a5f727d29e4b07220cc
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-10-23 22:34:30 -0400

    answer

---
+++ Palidrome/server.js
@@ -0,0 +1,80 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    if('student' in params){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      checkPalidrome = params['student'].toLowerCase().split('').reverse().join('')
+      if(params['student'] == checkPalidrome){
+        yesOrNo = 'Yes'
+        
+        }
+        else {
+          yesOrNo ='No'
+
+        }
+
+      const objToJson = {
+        yesOrNo: `${yesOrNo}`,
+        
+      }
+      res.end(JSON.stringify(objToJson));
+      
+      
+      //student = leon
+      //student != leon
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8001);
